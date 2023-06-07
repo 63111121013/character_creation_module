5f3d70914bf7fdf150adc190a9bcf182e34c57cb
Константин Куцырь <k3214891@yandex.ru>
2023-06-07 17:03:50 +0300

    Docstringed code

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    ...
+
+
+def defence(char_name: str, char_class: str) -> str:
+    ...
+
+
+def special(char_name: str, char_class: str) -> str:
+    ...
+
+
+def start_training(char_name: str, char_class: str) -> str:
+    ...
+
+
+def choice_char_class() -> str:
+    ...
+
+if __name__ == '__main__':
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений!')
+    print('Прежде чем начать игру...')
+    char_name: str = input('...назови себя: ')
+    print(f'Здравствуй, {char_name}! '
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+    print('Ты можешь выбрать один из трёх путей силы:')
+    print('Воитель, Маг, Лекарь')
+    char_class: str = choice_char_class()
+    print(start_training(char_name, char_class))
+
+
+def attack(char_name: str, char_class: str) -> str:
+    """Attack module, calculate class damage."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +48,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Defence module, calculate class armor."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +58,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Ultimate module, calculate type and power of calss special ability."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -35,6 +71,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Training module, testing the capabilities of the class."""
     if char_class == 'warrior':
         print(f'{char_name}, ты {char_class} — '
               f'отличный боец ближнего боя.')
@@ -62,6 +99,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Class selection module."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -85,6 +123,7 @@ def choice_char_class() -> str:
 
 
 def main():
+    """Functions unit module."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
